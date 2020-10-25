21dfc89d84053eca8a94e056d017777627afeebb
kitatora <mina3.ryu0728@gmail.com>
2020-10-26 03:50:08 +0900

    20201025

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Post
+from django import forms
+from .models import Post, Category
+
 
 admin.site.register(Post)
+admin.site.register(Category)
 
+class PostForm(forms.Form):
+    category_data = Category.objects.all()
+    category_choice = {}
+    for category in category_data:
+        category_choice[category] = category
+
+    title = forms.CharField(max_length=50, label='タイトル')
+    category = forms.ChoiceField(label='カテゴリ', widget=forms.Select, choices=list(category_choice.items())) # 追加
+    content = forms.CharField(label='内容', widget=forms.Textarea())
+    image = forms.ImageField(label='イメージ画像', required=False)
+#
 # Register your models here.
