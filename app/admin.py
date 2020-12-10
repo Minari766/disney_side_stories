77effb36fb746d8148c2872d6f56fc3c727cd5e0
kitatora <mina3.ryu0728@gmail.com>
2020-12-11 00:00:55 +0900

    20201210

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django import forms
-from .models import Post, Category
+from .models import Post, Category, Category2
 
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Category2)
 
 class PostForm(forms.Form):
     category_data = Category.objects.all()
