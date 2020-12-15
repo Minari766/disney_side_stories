5c29333b720fdb19ccfdcc3c2ded98eb7cc94017
kitatora <mina3.ryu0728@gmail.com>
2020-12-15 23:43:35 +0900

    20201215

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import View
 from django.shortcuts import render,redirect
 from .models import Post
-# from .models import Post, Area, Attraction, Category
+from .models import Post, Area, Attraction, Category
 from .forms import PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -35,12 +35,12 @@ class CreatePostView(LoginRequiredMixin, View):
             post_data = Post()
             post_data.author = request.user
             post_data.title = form.cleaned_data['title']
-            # area = form.cleaned_data['area']
-            # area_data = Area.objects.get(name=area)
-            # attraction = form.cleaned_data['attraction']
-            # attraction_data = Attraction.objects.get(name=attraction)
-            # category = form.cleaned_data['category']
-            # category_data = Attraction.objects.get(name=category)
+            area = form.cleaned_data['area']
+            area_data = Area.objects.get(name=area)
+            attraction = form.cleaned_data['attraction']
+            attraction_data = Attraction.objects.get(name=attraction)
+            category = form.cleaned_data['category']
+            category_data = Attraction.objects.get(name=category)
             post_data.content = form.cleaned_data['content']
             if request.FILES:
                 post_data.image = request.FILES.get('image')
@@ -58,9 +58,9 @@ class PostEditView(LoginRequiredMixin, View):
             request.POST or None,
             initial={
                 'title': post_data.title,
-                # 'area': post_data.area,
-                # 'attraction': post_data.attraction,
-                # 'category': post_data.category,
+                'area': post_data.area,
+                'attraction': post_data.attraction,
+                'category': post_data.category,
                 'content': post_data.content,
                 'image': post_data.image,
             }
@@ -76,15 +76,15 @@ class PostEditView(LoginRequiredMixin, View):
         if form.is_valid():
             post_data = Post.objects.get(id=self.kwargs['pk'])
             post_data.title = form.cleaned_data['title']
-            # area = form.cleaned_data['area']
-            # area_data = Area.objects.get(name=area)
-            # post_data.area = area_data
-            # attraction = form.cleaned_data['attraction']
-            # attraction_data = Attraction.objects.get(name=attraction)
-            # post_data.attraction = attraction_data
-            # category = form.cleaned_data['category']
-            # category_data = Attraction.objects.get(name=category)
-            # post_data.category = category_data
+            area = form.cleaned_data['area']
+            area_data = Area.objects.get(name=area)
+            post_data.area = area_data
+            attraction = form.cleaned_data['attraction']
+            attraction_data = Attraction.objects.get(name=attraction)
+            post_data.attraction = attraction_data
+            category = form.cleaned_data['category']
+            category_data = Attraction.objects.get(name=category)
+            post_data.category = category_data
             post_data.content = form.cleaned_data['content']
             if request.FILES:
                 post_data.image = request.FILES.get('image')
@@ -122,15 +122,15 @@ class CreatePostView(LoginRequiredMixin, View):
             post_data = Post()
             post_data.author = request.user
             post_data.title = form.cleaned_data['title']
-            # area = form.cleaned_data['area']
-            # area_data = Area.objects.get(name=area)
-            # post_data.area = area_data
-            # attraction = form.cleaned_data['attraction']
-            # attraction_data = Attraction.objects.get(name=attraction)
-            # post_data.attraction = attraction_data
-            # category = form.cleaned_data['category']
-            # category_data = Area.objects.get(name=category)
-            # post_data.category = category_data
+            area = form.cleaned_data['area']
+            area_data = Area.objects.get(name=area)
+            post_data.area = area_data
+            attraction = form.cleaned_data['attraction']
+            attraction_data = Attraction.objects.get(name=attraction)
+            post_data.attraction = attraction_data
+            category = form.cleaned_data['category']
+            category_data = Area.objects.get(name=category)
+            post_data.category = category_data
             post_data.content = form.cleaned_data['content']
             if request.FILES:
                 post_data.image = request.FILES.get('image')
@@ -141,29 +141,29 @@ class CreatePostView(LoginRequiredMixin, View):
             'form': form
         })
 
-# class AreaView(View):
-#     def get(self, request, *args, **kwargs):
-#         area_data = Area.objects.get(name=self.kwargs['area'])
-#         post_data = Post.objects.order_by('-id').filter(area=area_data)
-#         return render(request, 'app/index.html', {
-#             'post_data': post_data
-#         })
+class AreaView(View):
+    def get(self, request, *args, **kwargs):
+        area_data = Area.objects.get(name=self.kwargs['area'])
+        post_data = Post.objects.order_by('-id').filter(area=area_data)
+        return render(request, 'app/index.html', {
+            'post_data': post_data
+        })
 
-# class AttractionView(View):
-#     def get(self, request, *args, **kwargs):
-#         attraction_data = Attraction.objects.get(name=self.kwargs['attraction'])
-#         post_data = Post.objects.order_by('-id').filter(attraction=attraction_data)
-#         return render(request, 'app/index.html', {
-#             'post_data': post_data
-#         })
+class AttractionView(View):
+    def get(self, request, *args, **kwargs):
+        attraction_data = Attraction.objects.get(name=self.kwargs['attraction'])
+        post_data = Post.objects.order_by('-id').filter(attraction=attraction_data)
+        return render(request, 'app/index.html', {
+            'post_data': post_data
+        })
 
-# class CategoryView(View):
-#     def get(self, request, *args, **kwargs):
-#         category_data = Category.objects.get(name=self.kwargs['category'])
-#         post_data = Post.objects.order_by('-id').filter(category=category_data)
-#         return render(request, 'app/index.html', {
-#             'post_data': post_data
-#         })
+class CategoryView(View):
+    def get(self, request, *args, **kwargs):
+        category_data = Category.objects.get(name=self.kwargs['category'])
+        post_data = Post.objects.order_by('-id').filter(category=category_data)
+        return render(request, 'app/index.html', {
+            'post_data': post_data
+        })
 
 class BazaarView(View):
     def get(self, request, *args, **kwargs):
