bd752be4072848df971ba03bfb2c7cbcde7dc8eb
kitatora <mina3.ryu0728@gmail.com>
2020-12-16 09:31:04 +0900

    20201216

--- app/forms.py
+++ app/forms.py
@@ -1,25 +1,25 @@
 from django import forms
-# from .models import Area, Attraction, Category
+from .models import Area, Attraction, Category
 
 class PostForm(forms.Form):
-    # area_data = Area.objects.all()
-    # area_choice = {}
-    # for area in area_data:
-    #     area_choice[area] = area
+    area_data = Area.objects.all()
+    area_choice = {}
+    for area in area_data:
+        area_choice[area] = area
     
-    # attraction_data = Attraction.objects.all()
-    # attraction_choice = {}
-    # for attraction in attraction_data:
-    #     attraction_choice[attraction] = attraction
+    attraction_data = Attraction.objects.all()
+    attraction_choice = {}
+    for attraction in attraction_data:
+        attraction_choice[attraction] = attraction
 
-    # category_data = Category.objects.all()
-    # category_choice = {}
-    # for category in category_data:
-    #     category_choice[category] = category
+    category_data = Category.objects.all()
+    category_choice = {}
+    for category in category_data:
+        category_choice[category] = category
 
     title = forms.CharField(max_length=50, label='タイトル')
-    # area = forms.ChoiceField(label='エリア', widget=forms.Select, choices=list(area_choice.items()))
-    # attraction = forms.ChoiceField(label='アトラクション', widget=forms.Select, choices=list(attraction_choice.items()))
-    # category = forms.ChoiceField(label='カテゴリ', widget=forms.Select, choices=list(category_choice.items()))
+    area = forms.ChoiceField(label='エリア', widget=forms.Select, choices=list(area_choice.items()))
+    attraction = forms.ChoiceField(label='アトラクション', widget=forms.Select, choices=list(attraction_choice.items()))
+    category = forms.ChoiceField(label='カテゴリ', widget=forms.Select, choices=list(category_choice.items()))
     content = forms.CharField(label='内容', widget=forms.Textarea())
     image = forms.ImageField(label='イメージ画像', required=False)
