a7ecc13913ca6bf1febd9c53bb3049d59a35d770
kitatora <mina3.ryu0728@gmail.com>
2021-01-18 00:21:10 +0900

    20210118

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django import forms
-from .models import Post, Area, Attraction, Category, Like
+from .models import Post, Area, Attraction, Category, Like, ProfImage
 
 
 admin.site.register(Post)
@@ -8,6 +8,7 @@ admin.site.register(Area)
 admin.site.register(Attraction)
 admin.site.register(Category)
 admin.site.register(Like)
+admin.site.register(ProfImage)
 
 #
 # Register your models here.
