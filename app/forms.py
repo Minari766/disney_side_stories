9758cea35af922920e3547dbec06a58d8f839d72
kitatora <mina3.ryu0728@gmail.com>
2021-08-15 15:23:58 +0900

    20210815

--- app/forms.py
+++ app/forms.py
@@ -3,38 +3,38 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.mail import BadHeaderError, message, send_mail
 from django.http import HttpResponse
-from .models import Area, Attraction, Category
+# from .models import Area, Attraction, Category
 
 
 class PostForm(forms.Form):
-    # pass
-    area_data = Area.objects.all()
-    area_choice = {}
-    for area in area_data:
-        area_choice[area] = area
+    pass
+    # area_data = Area.objects.all()
+    # area_choice = {}
+    # for area in area_data:
+    #     area_choice[area] = area
     
-    attraction_data = Attraction.objects.all()
-    attraction_choice = {}
-    for attraction in attraction_data:
-        attraction_choice[attraction] = attraction
+    # attraction_data = Attraction.objects.all()
+    # attraction_choice = {}
+    # for attraction in attraction_data:
+    #     attraction_choice[attraction] = attraction
 
-    category_data = Category.objects.all()
-    category_choice = {}
-    for category in category_data:
-        category_choice[category] = category
+    # category_data = Category.objects.all()
+    # category_choice = {}
+    # for category in category_data:
+    #     category_choice[category] = category
     
-    title = forms.CharField(max_length=50, label='タイトル')
-    area = forms.ChoiceField(label='エリア', widget=forms.Select, choices=list(area_choice.items()), initial="----選択してください")
-    attraction = forms.ChoiceField(label='アトラクション', widget=forms.Select, choices=list(attraction_choice.items()))
-    category = forms.ChoiceField(label='カテゴリ', widget=forms.Select, choices=list(category_choice.items()), initial="----選択してください")
-    content = forms.CharField(label='内容', widget=forms.Textarea())
-    image = forms.ImageField(label='イメージ画像', required=False)
+    # title = forms.CharField(max_length=50, label='タイトル')
+    # area = forms.ChoiceField(label='エリア', widget=forms.Select, choices=list(area_choice.items()), initial="----選択してください")
+    # attraction = forms.ChoiceField(label='アトラクション', widget=forms.Select, choices=list(attraction_choice.items()))
+    # category = forms.ChoiceField(label='カテゴリ', widget=forms.Select, choices=list(category_choice.items()), initial="----選択してください")
+    # content = forms.CharField(label='内容', widget=forms.Textarea())
+    # image = forms.ImageField(label='イメージ画像', required=False)
     
-    def clean_name(self):
-        area = self.cleaned_data.get('area')
-        if area in ('ワールドバザール'):
-            self.add_error('area', 'お名前に禁止ワードが含まれています')
-        return area
+    # def clean_name(self):
+    #     area = self.cleaned_data.get('area')
+    #     if area in ('ワールドバザール'):
+    #         self.add_error('area', 'お名前に禁止ワードが含まれています')
+    #     return area
 
 CATEGORIES = (
     ('0', '----選択してください'),
